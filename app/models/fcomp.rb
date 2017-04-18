e7652bccbb5c64a695d6770830b6381ed0f04784
Andrea <lake_0f_tearz@yahoo.com>
2017-04-18 21:13:53 +0200

    ok form for new comp with image

--- app/models/fcomp.rb
+++ app/models/fcomp.rb
@@ -1,5 +1,6 @@
 class Fcomp < ApplicationRecord
   default_scope -> { order(:name) }
+    mount_uploader :logo, LogoUploader
   validates :name, presence: true
   validates :country, presence: true
   validates :webpage, presence: true

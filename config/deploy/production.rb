728e8a2dd1ce45f7c5bd4d774a69c2c8838ada9b
Harald Eilertsen <haraldei@anduin.net>
2017-05-21 16:23:03 +0200

    Update deployment config.
    Assume separate user for app, deploy to app users home dir.
    Instruct puma to run as app user.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,4 +3,4 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "volse.anduin.net", user: "andrea", roles: %w{app db web}
+server "volse.anduin.net", user: "trvguit", roles: %w{app db web}
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,5 +3,5 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "volse.local", user: "andrea", roles: %w{app db web}
+server "volse.local", user: "trvguit", roles: %w{app db web}
 set :rails_env, :production
